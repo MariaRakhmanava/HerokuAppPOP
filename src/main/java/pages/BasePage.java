45de0ba6eac23407f8a35e2aef76c7ffe0374f44
MariaRakhmanava <mariah.rakhmanava@gmail.com>
2021-05-24 19:56:00 +0300

    Added pages and tests .java files, the gitignore file, HerokuAppFile uploaded file

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -1,11 +1,20 @@
 package pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 abstract class BasePage {
     WebDriver driver;
+    WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    public void waitForElementLocated(By locator, int timeout) {
+        wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
